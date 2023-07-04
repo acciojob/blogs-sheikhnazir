29f6a18357cbc0cb5f97a4914ef07027c0c7f4cf
SheikhNazir <73103801+naazie@users.noreply.github.com>
2023-07-04 11:32:32 +0530

    modified service layer

--- src/main/java/com/driver/controller/ImageController.java
+++ src/main/java/com/driver/controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.driver.controller;
 
+import com.driver.services.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
